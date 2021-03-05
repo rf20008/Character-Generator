64b3622da61fcc12cf48d72022d570db17958329
duck_master <duckmaster0@protonmail.com>
2021-03-04 21:06:32 -0500

    updated script + added json-documentation.txt
    - gutted hacky random feature selection thing 
    - moved giant comment to json-documentation.txt
    - tested code properly

--- rf20008_gen_in_progress.py
+++ rf20008_gen_in_progress.py
@@ -19,6 +19,7 @@ charAttributes = {"region": None,
                   "age": None}
 
 #custom error types
+#TODO: eliminate or make more pythonic
 class NonExistingAttributeError(AttributeError):
     '''An attribute for the character doesn't exist!'''
     pass
@@ -26,69 +27,7 @@ class InvalidDataError(Exception):
     '''The data is invalid.'''
     pass
 
-#TODO: move 
-##Species (maybe later)
-##Region,
-##      Percentage of people in this region,
-##  
-##      Genders (names),
-##          GenderName:
-##              Percentage with this gender
-##              Names,
-##                  Name,
-##                  Percentage
-##        Religions (names),
-##          Religion Name, Percentage
-##          Percentage with this religion (in this region)
-##        Political Parties (names),
-##          Party name, Percentage  
-##          Percentage with this party (in this region)
-##        Jobs,
-##          Percentage of people with this job
-##        Ages,
-##          Percentage of people with this age
-##example:
-##{
-##  "Data_testregion1": {
-##    "population": 99.0,
-##    "Genders": {
-##      "Male": {
-##        "population": 50.0, "Names": {
-##          "Matthew":20.0, "Andrew": 19.0, "David": 21.0, "Seth": 10.0, "Riley": 10.0, "Luke": 20.0
-##        }
-##      },
-##      "Female": {
-##        "population": 49.0, "Names": {
-##            "Ophelia": 80.0, "Olivia": 20.0
-##          }
-##        },
-##      "O": {
-##        "population": 1.0, "Names": {
-##          "O": 100.0
-##        }
-##      }
-##      },
-##      "Religions": {
-##        "Christian": 100.0,
-##      },
-##      "Political Parties": {
-##        "Democratic": 50.0, "Republican": 50.0
-##      },
-##      "Jobs": {
-##        "Job1": 20.0, "Job2": 80.0
-##      },
-##      "Ages": {
-##        10: 100.0
-##      }
-##    }
-##}
-
-#repeat for each region
-regionsList= list(data.keys())
-for region in regionsList:
-    populationsDict[region] = data[region]["population"]
-    
-
+#selects from categorical distribution as represented in dictionary
 #code credit to StackOverflow user "Boojum"
 #https://stackoverflow.com/questions/3655430/selection-based-on-percentage-weighting
 def select(values):
@@ -103,134 +42,31 @@ def select(values):
     return item # Shouldn't get here, but just in case of rounding...
 
 #TODO: simplify code
-#first for region
-n = random.random()
-m = 0
-i = -1
-
+#TODO: implement random trait selection
+#repeat for each region
+regionsList= list(data.keys())
 for region in regionsList:
-    i += 1
-    if n > m:
-        charAttributes["region"] = region
-        break
-    else:
-        m += data[region]["population"]
-        if i + 1 == len(regionsList):
-            #I will need to implement the gss thing later, for now it is raise an error
-            raise InvalidDataError
-        
-
-#next we do Gender
-n = random.random()
-gendersList = list(data[charAttributes["region"]]["Genders"].keys())
-i = -1
-m=0
-for gender in gendersList:
-    #print(gender)
+    populationsDict[region] = data[region]["population"]
     
-    i += 1
-    #print(data[region]["Genders"][gender])
-    if n > m:
-        charAttributes["gender"] = gender
-        break
-    else:
-        m += data[charAttributes["region"]]["Genders"][gender]["population"]/100 #next one!
-        if i + 1 == len(gendersList):
-            #I will need to implement the gss thing later, for now it is raise an error
-            raise InvalidDataError
 
-
-
-#now, Names
-NamesList = list(data[charAttributes["region"]]["Genders"][charAttributes["gender"]]["Names"].keys())
-n = random.random()
-i = -1
-m=0
-for name in NamesList:
-    #print(gender)
-    
-    i += 1
-    #print(data[region]["Genders"][gender])
-    if n > m:
-        charAttributes["name"]=name
-        break
-    else:
-        m += data[charAttributes["region"]]["Genders"][charAttributes["gender"]]["Names"][name]/100 #next one!
-        if i + 1 == len(NamesList):
-            #we will need to implement the gss thing later, for now it is raise an error
-            raise InvalidDataError
-
-#fourthly, religion
-ReligionsList = list(data[charAttributes["region"]]["Religions"].keys())
-n = random.random()
-i = -1
-m=0
-for religion in ReligionsList:
-    #print(gender)
-    i += 1
-    #print(data[region]["Genders"][gender])
-    if n > m:
-        charAttributes["religion"]=religion
-        break
-    else:
-        m += data[charAttributes["region"]]["Religions"][religion]/100 #next one!
-        if i + 1 == len(ReligionsList):
-            #we will need to implement the gss thing later, for now it is raise an error
-            raise InvalidDataError
-
-
-
-
-#fifthly, political party (assuming political party is independent of religion, this is not the case)
-PoliticalPartiesList = list(data[charAttributes["region"]]["Political Parties"].keys())
-n = random.random()
-i = -1
-m=0
-for party in PoliticalPartiesList:
-    #print(gender)
-    i += 1
-    if n > m: 
-        charAttributes["political party"]=party
-        break
-    else:
-        m += data[charAttributes["region"]]["Political Parties"][party]/100 #next one!
-        if i + 1 == len(PoliticalPartiesList):
-            #we will need to implement the gss thing later, for now it is raise an error
-            raise InvalidDataError
-        
-
-#sixthly, Jobs
-JobsList = list(data[charAttributes["region"]]["Jobs"].keys())
-n = random.random()
-i = -1
-m=0
-for job in JobsList:
-    #print(gender)
-    i += 1
-    if n > m:
-        charAttributes["job"]=job
-        break
-    else:
-        m += data[charAttributes["region"]]["Jobs"][job]/100 #next one!
-        if i + 1 == len(JobsList):
-            #we will need to implement the gss thing later, for now it is raise an error
-            raise InvalidDataError
-
-
-#finally, ages
-AgesList = list(data[charAttributes["region"]]["Ages"].keys())
-n = random.random()
-i = -1
-m=0
-for age in AgesList:
-    #print(gender)
-    i += 1
-    if n > m:
-        charAttributes["age"]=age
-        break
-    else:
-        m += data[charAttributes["region"]]["Ages"][age]/100 #next one!
-        if i + 1 == len(AgesList):
-            #we will need to implement the gss thing later, for now it is raise an error
-            raise InvalidDataError
-print(charAttributes)
+#first for region
+print(f'regionsList = {regionsList}') #debug
+###next we do Gender
+##gendersList = list(data[charAttributes["region"]]["Genders"].keys())
+##print(f'gendersList = {gendersList}') #debug
+###now, Names
+##NamesList = list(data[charAttributes["region"]]["Genders"][charAttributes["gender"]]["Names"].keys)
+##print(f'NamesList = {NamesList}') #debug
+###fourthly, religion
+##ReligionsList = list(data[charAttributes["region"]]["Religions"].keys())
+##print(f'ReligionsList = {ReligionsList}') #debug
+###fifthly, political party
+###assuming political party is independent of religion
+##PoliticalPartiesList = list(data[charAttributes["region"]]["Political Parties"].keys())
+##print(f'PoliticalPartiesList = {PoliticalPartiesList}') #debug
+###sixthly, Jobs
+##JobsList = list(data[charAttributes["region"]]["Jobs"].keys())
+##print(f'JobsList = {JobsList}') #debug
+###finally, ages
+##AgesList = list(data[charAttributes["region"]]["Ages"].keys())
+##print(f'AgesList = {AgesList}') #debug
